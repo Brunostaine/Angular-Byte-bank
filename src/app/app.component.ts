f9ed2af381c7509fe2470c36d75acc8d4824a3ed
Bruno Staine <brunostaine@hotmail.com>
2022-02-19 15:56:09 -0300

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'byte-bank';
+  title = 'bytebank';
   transferencias: any[] = [];
 
+  constructor(private service: TransferenciaService) {}
+
   transferir($event) {
-    console.log($event);
-    const transferencia = {...$event, data: new Date()}
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event);
   }
 }
 
